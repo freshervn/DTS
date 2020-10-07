ab48836524317159e5e9e68f58db9643fd3a7819
freshervn <40496285+freshervn@users.noreply.github.com>
2020-10-07 23:16:49 +0700

    ahihi

--- truongphathome/truongphathome.js
+++ truongphathome/truongphathome.js
@@ -1,5 +1,18 @@
-    // auto rize;
-      $(function(){
+      // nextslide
+      var slideIndex = 0;
+      var slides = ["images/home/slider2_u1.jpg","images/home/slider1_u22.jpg", "images/home/slider3_u2.jpg"];
+      function plusSlides(n) {
+        slideIndex += n;
+        if (slideIndex < 0) { slideIndex = slides.length - 1 };
+        if (slideIndex >= slides.length) { n = 0 };
+        showSlides(slideIndex);
+      }
+      function showSlides(slideIndex) {
+        document.getElementsByClassName("overview")[0].setAttribute("style", "background-image: url(" + slides[slideIndex] + ");background-size: cover;");
+      }
+     
+     //       
+    $(function(){
     var textArea = $('#content'),
     hiddenDiv = $(document.createElement('div')),
     content = null;
@@ -16,4 +29,6 @@
         $(this).css('height', hiddenDiv.height());
     });
 });
+      
+
       
