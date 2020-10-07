3ed6d44148e2e0b470a39570194b0178dae473e3
freshervn <40496285+freshervn@users.noreply.github.com>
2020-10-07 22:37:29 +0700

    cung oke day

--- truongphathome/truongphathome.js
+++ truongphathome/truongphathome.js
@@ -1,14 +1,19 @@
-      // nextslide
-      var slideIndex = 0;
-      var slides = ["images/home/slider2_u1.jpg","images/home/slider1_u22.jpg", "images/home/slider3_u2.jpg"];
-      function plusSlides(n) {
-        slideIndex += n;
-        if (slideIndex < 0) { slideIndex = slides.length - 1 };
-        if (slideIndex >= slides.length) { n = 0 };
-        showSlides(slideIndex);
-      }
-      function showSlides(slideIndex) {
-        document.getElementsByClassName("overview")[0].setAttribute("style", "background-image: url(" + slides[slideIndex] + ");background-size: cover;");
-      }
-      //tranlate
+    // auto rize;
+      $(function(){
+    var textArea = $('#content'),
+    hiddenDiv = $(document.createElement('div')),
+    content = null;
+    
+    textArea.addClass('noscroll');
+    hiddenDiv.addClass('hiddendiv');
+    
+    $(textArea).after(hiddenDiv);
+    
+    textArea.on('keyup', function(){
+        content = $(this).val();
+        content = content.replace(/\n/g, '<br>');
+        hiddenDiv.html(content + '<br class="lbr">');
+        $(this).css('height', hiddenDiv.height());
+    });
+});
       
